445b12bbc0d144f3a1b1022c59d89c8cf523440c
buggyCoder269 <Pranay.Shah1@ibm.com>
2026-07-19 18:33:29 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Axiom Project</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Mulish', sans-serif",
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* <Provider store={store}> */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    {/* </Provider> */}
+  </StrictMode>,
+)
